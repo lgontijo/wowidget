ab63789511f6a2b4c4233a359ded46012f27a080
Luiz <luizgontijodm@gmail.com>
2018-12-27 12:31:03 -0800

    Implement prettier

--- public/js/getNews.js
+++ public/js/getNews.js
@@ -3,7 +3,6 @@ let theNewApiTitles = [];
 let theNewApiImg = [];
 let theNewApiSource = [];
 
-
 // getNews need to wait until fire() runs in order to collect the API key.
 setTimeout(getNews, 3000);
 
@@ -11,41 +10,37 @@ setTimeout(getNews, 3000);
 fire();
 
 // This function calls the News API and parses through the response and assigns values to the global variables.
-function getNews(country = 'br'){
+function getNews(country = "br") {
+  const newsApiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${news_api_key}`;
 
-	const newsApiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${news_api_key}`;
+  // Retriving everything from the newAPI
+  $.get(newsApiUrl, function(data) {
+    // holds all of the articles. Its type is an object
+    let articles = data.articles;
 
-	// Retriving everything from the newAPI
-	$.get(newsApiUrl, function(data){
+    // Retrives the entire article
+    articles.forEach(function(element) {
+      theNewApiUrls.push(element.article);
+    });
 
-		// holds all of the articles. Its type is an object
-		let articles = data.articles;
+    // Retrives the news urls
+    articles.forEach(function(element) {
+      theNewApiUrls.push(element.url);
+    });
 
-		// Retrives the entire article
-		articles.forEach(function(element){
-			theNewApiUrls.push(element.article);
-		})
+    // Retrives the news titles
+    articles.forEach(function(element) {
+      theNewApiTitles.push(element.title);
+    });
 
-		// Retrives the news urls
-		articles.forEach(function(element){
-			theNewApiUrls.push(element.url);
-		})
+    // Retrives the news images
+    articles.forEach(function(element) {
+      theNewApiImg.push(element.urlToImage);
+    });
 
-		// Retrives the news titles
-		articles.forEach(function(element){
-			theNewApiTitles.push(element.title);
-		})
-
-		// Retrives the news images
-		articles.forEach(function (element){
-			theNewApiImg.push(element.urlToImage);
-		})
-
-		// Retrives the news sources
-		articles.forEach(function(element){
-			theNewApiSource.push(element.source.name);
-		})
-
-	});
-
+    // Retrives the news sources
+    articles.forEach(function(element) {
+      theNewApiSource.push(element.source.name);
+    });
+  });
 }
